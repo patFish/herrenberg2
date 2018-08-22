9b805e9290cff507ed6ff47310d1d20ff4a9a79d
p.fischer <p.fischer@accenture.com>
2018-08-22 20:46:33 +0200

    initial draft

---
+++ src/components/Content.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import BeerOverview from './BeerOverview';
+
+class Content extends Component {
+  render() {
+    return (
+      <div>
+        <BeerOverview />
+      </div>
+    );
+  }
+}
+
+export default Content;
