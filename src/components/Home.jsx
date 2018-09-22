88845360293225cb3525ee3e163f8b0fdf1c9607
p.fischer <p.fischer@accenture.com>
2018-09-22 17:20:29 +0100

    added new landing picture

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import '../styles/Home.css';
-import nuglar from '../assets/Nuglar_Pantaleon.jpg';
-import sortiment from '../assets/sortiment.png';
+import nuglar from '../assets/landingpicture.png';
+// import sortiment from '../assets/sortiment.png';
 
 class Home extends Component {
   render() {
     return (
       <div>
         <img className="HomeBackground" src={nuglar} alt="Herrenberg" />
-        <img src={sortiment} className="beer" />
+        {/* <img src={sortiment} className="beer" /> */}
       </div>
     );
   }
